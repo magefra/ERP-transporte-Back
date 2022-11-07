739f62d53c29b739822a0934228011ab830cbe9c
magefra9 <magefra9@hotmail.com>
2022-11-07 15:55:20 -0600

    Agregando TractorRemolqueDolly

---
+++ Core/ERPTransporteBack.Core.Domain/Entities/Catalogos/TractorRemolqueDolly.cs
@@ -0,0 +1,82 @@
+﻿namespace ERPTransporteBack.Core.Domain.Entities.Catalogos
+{
+    public class TractorRemolqueDolly
+    {
+        public string NumeroEconomico { get; set; }
+        public string SEG { get; set; }
+        public string VFC { get; set; }
+        public string TCA { get; set; }
+        public string IFM { get; set; }
+        public string CER { get; set; }
+        public string LecturaInicial { get; set; }
+        public double KMsLecturaActual { get; set; }
+        public double KMsAcoumulados { get; set; }
+        public double KMsPromDiario { get; set; }
+        public double ListrosCargados { get; set; }
+        public double Rendimiento { get; set; }
+        public int HorasServicio { get; set; }
+        public string Propiedad { get; set; }
+        public string Placa { get; set; }
+        public string Status { get; set; }
+        public string NumeroExterno { get; set; }
+        public string Servicio { get; set; }
+        public string Referencia { get; set; }
+        public string PermisoSCT { get; set; }
+        public string Certificado { get; set; }
+        public string TipoCarga { get; set; }
+        public string ClaveOperador { get; set; }
+        public string Conductor { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Circuito { get; set; }
+        public string DescripcionCircuito { get; set; }
+        public string CentroCosto { get; set; }
+        public string LineaNegocio { get; set; }
+        public string TipoUnidad { get; set; }
+        public string Ano { get; set; }
+        public string PermisoCST2 { get; set; }
+        public string NumeroSerie { get; set; }
+        public string NumeroFabricante { get; set; }
+        public string NumeroMotor { get; set; }
+        public int NumeroEjes { get; set; }
+        public string SuspencionEDireccion { get; set; }
+        public string SuspencionEMotriz { get; set; }
+        public string SupencionEArrast { get; set; }
+        public string TarjetaPeaje { get; set; }
+        public string RastreoSatelital { get; set; }
+        public string StatusAntena { get; set; }
+        public string TarjetaCombustible { get; set; }
+        public string Longitud { get; set; }
+
+        public string ValorAdquisicion { get; set; }
+        public string Factura { get; set; }
+        public DateTime FechaCompra { get; set; }
+
+        public DateTime FechaBaja { get; set; }
+
+        public string ClaveBancaCivil { get; set; }
+        public string NumeroPolizaSeguro { get; set; }
+        public string Aseguradora { get; set; }
+        public DateTime VigenciaSeguroInicio { get; set; }
+        public DateTime VigenciaSeguroFinal { get; set; }
+        public decimal CostoSeguro { get; set; }
+        public string AseguradoraMedioAmbiente { get; set; }
+        public string ClaveVerificacion { get; set; }
+        public DateTime VigenciaVerificacionInicio { get; set; }
+        public DateTime VigenciaVerificacionFinal { get; set; }
+        public string TarjetaCirculacion { get; set; }
+        public double RendimientoAutorizado1KMLitro { get; set; }
+        public double RendimientoAutorizado2KMLitro { get; set; }
+        public double RendimientoAutorizado3KMLitro { get; set; }
+        public double FactorAjusteCombustible { get; set; }
+        public string Combustible { get; set; }
+        public int CapacidadTanque { get; set; }
+        public string Tanque { get; set; }
+        public string NumeroEconomicoRemolque1 { get; set; }
+        public string NumeroEconomicoDolly1 { get; set; }
+        public string NumeroEconomicoRemolque2 { get; set; }
+
+
+
+    }
+}
