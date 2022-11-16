f7080fd87b667d196e4aacd7b45ec0ed2c91bd26
magefra9 <magefra9@hotmail.com>
2022-11-16 09:41:01 -0600

    Agregando una mejor BaseEntity y agregando contracto de repositorio

--- Core/ERPTransporteBack.Core.Domain/Entities/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ERPTransporteBack.Core.Domain.Entities
-{
-    public abstract class BaseEntity<TKey>
-    {
-        public TKey Id { get; set; }
-        public DateTime CreatedAtUtc { get; set; }
-        public DateTime? LastModifiedAtUtc { get; set; }
-    }
-}
---
+++ Core/ERPTransporteBack.Core.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace ERPTransporteBack.Core.Domain.Entities
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedDate { get; set; }
+        public DateTime LastModifieDate { get; set; }
+
+    }
+}
